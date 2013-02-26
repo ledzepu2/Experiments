ff4eea572c5d6803a011c9f64f4f16f3e263eb98
Arun Menon <ledzepu2@gmail.com>
2013-02-26 16:32:25 +0530

    Made changes to Git Test

--- Experiments/src/com/espn/experiments/GitTest.java
+++ Experiments/src/com/espn/experiments/GitTest.java
@@ -3,6 +3,6 @@ package com.espn.experiments;
 public class GitTest {
 
 	public static void main(String[] args) {
-	    System.out.println("Git is cool");
+	    System.out.println("Git is cool. I want to learn it");
 	  }
 }
